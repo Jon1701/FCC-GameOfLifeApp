2b25d430b12cd52b8ec1d8435b47777bffb5ef0e
Jon Balon <development@jonbalon.com>
2016-08-11 18:11:41 -0400

    Updated Cell component to change background colour when state changes

--- source/javascript/GameOfLife/Cell.jsx
+++ source/javascript/GameOfLife/Cell.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import classNames from 'classnames';
+
 class Cell extends React.Component {
 
   // Component Constructor.
@@ -25,8 +27,17 @@ class Cell extends React.Component {
   // Component Render.
   render() {
 
+    // Classes used for this cell.
+    //
+    // If this.props.contents flips between true/false,
+    // add/remove the .cell-active class.
+    var cellClasses = classNames({
+      'cell': true,
+      'cell-active': this.props.contents
+    });
+
     return (
-      <div className="cell" onClick={this.update.bind(this)}>
+      <div className={cellClasses} onClick={this.update.bind(this)}>
         {String(this.props.contents)}
       </div>
     )
