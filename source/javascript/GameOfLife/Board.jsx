d9603522bed2b903ae1aa90244804a6da026feac
Jon Balon <development@jonbalon.com>
2016-08-12 01:27:25 -0400

    Started work on case 1 algorithm

--- source/javascript/GameOfLife/Board.jsx
+++ source/javascript/GameOfLife/Board.jsx
@@ -57,7 +57,95 @@ class Board extends React.Component {
 
   scanner() {
 
+    var generateScanRadius = function(grid, i, j) {
+      // Get top-left value.
+      var item0 = null;
+      try {
+        item0 = grid[i-1][j-1];
+      } catch(e) {
+        item0 = null;
+      }
+
+      // Get top-middle value.
+      var item1 = null;
+      try {
+        item1 = grid[i][j-1];
+      } catch(e) {
+        item1 = null;
+      }
+
+      // Get top-right value.
+      var item2 = null;
+      try {
+        item2 = grid[i+1][j-1];
+      } catch(e) {
+        item2 = null;
+      }
+
+      // Get middle-left value.
+      var item3 = null;
+      try {
+        item3 = grid[i-1][j];
+      } catch(e) {
+        item3 = null;
+      }
+
+      // Get origin value.
+      var item4 = null;
+      try {
+        item4 = grid[i][j];
+      } catch(e) {
+        item4 = null;
+      }
+
+      // Get middle-right value.
+      var item5 = null;
+      try {
+        item5 = grid[i+1][j];
+      } catch(e) {
+        item5 = null;
+      }
+
+      // Get bottom-left value.
+      var item6 = null;
+      try {
+        item6 = grid[i-1][j+1];
+      } catch(e) {
+        item6 = null;
+      }
+
+      // Get bottom-middle value.
+      var item7 = null;
+      try {
+        item7 = grid[i][j+1];
+      } catch(e) {
+        item7 = null;
+      }
+
+      // Get bottom-right value.
+      var item8 = null;
+      try {
+        item8 = grid[i+1][j+1];
+      } catch(e) {
+        item8 = null;
+      }
+
+      // Scan radius.
+      var scanRadius = {
+        indices: [
+                    [i-1, j-1], [i, j-1], [i+1, j-1],
+                    [i-1, j  ], [i, j  ], [i+1, j  ],
+                    [i-1, j+1], [i, j+1], [i+1, j+1]
+                 ],
+        values: [item0, item1, item2, item3, item4, item5, item6, item7, item8]
+      }
+
+      return scanRadius;
+    }
+
     var grid = this.state.grid.slice();
+
+    var gridCopy = this.state.grid.slice();
 
     // Go through the rows.
     for(var i=0; i<grid.length; i++) {
@@ -65,16 +153,55 @@ class Board extends React.Component {
       // Go through the columns.
       for(var j=0; j<grid[i].length; j++) {
 
-        // Flip cell state
-        grid[i][j] = !grid[i][j];
+        // For the current cell, generate a scan radius.
+        //
+        // Look at the state of cells around the current cell.
+        var scanRadius = generateScanRadius(grid, i, j);
 
+        // Index of origin cell.
+        var originIdx = 4;
+
+        // Number of cells which are active/inactive in the scan radius.
+        var numActiveCells = 0;
+        var numInactiveCells = 0;
+
+        // Grid indices of the active/inactive cells.
+        var idxActiveCells = [];
+        var idxInactiveCells = [];
+
+        // Get the number of cells in the scan radius which are active/inactive.
+        //
+        // scanRadius.values is an array which consists of either undefined,
+        // true, or false.
+        for(var x=0; x<scanRadius.values.length; x++) {
+          if (scanRadius.values[x] === true) {
+            numActiveCells++;
+            idxActiveCells.push(scanRadius.indices[x]);
+          } else if (scanRadius.values[x] == false) {
+            numInactiveCells++;
+            idxInactiveCells.push(scanRadius.indices[x]);
+          }
+        }
+
+        // Case 1: Any live cell with fewer than two live neighbours dies,
+        // as if caused by under-population.
+        if (numActiveCells < 2) {
+
+          // Flip cell state
+          gridCopy[i][j] = !grid[i][j];
+
+        }
+
+
+
+        //break;
       };
-
+      //break;
     };
 
     // Update grid.
     this.setState({
-      grid: grid
+      grid: gridCopy
     });
 
   }
